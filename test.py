592bbcf589380c9af4cfb554f80b8c2557963031
Mike Ridley <ridmic@gmail.com>
2017-12-02 15:58:07 +1100

    Changed to a code test

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print('hello world')
+
+
+def first_repeat_of_a_char(haystack):
+    found = []
+    for straw in haystack:
+        if straw in found:
+            return straw
+        found.append(straw)
+    return None
+
+
+chk1 = "ABCDFECBCDES"
+print( first_repeat_of_a_char(chk1))
